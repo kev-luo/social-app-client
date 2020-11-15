69fd2617602a1467e28f72396d1ba91d6e06028c
Kevin Luo <kvn.luo@gmail.com>
2020-11-14 20:12:01 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import ApolloProvider from './ApolloProvider';
+
+ReactDOM.render(
+  ApolloProvider, document.getElementById('root')
+);
---
+++ src/utils/MyPopup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Popup } from 'semantic-ui-react';
+
+export default function MyPopup({ content, children }) {
+  return (
+    <Popup
+      content={ content }
+      inverted
+      trigger={ children }
+    />
+  )
+}
